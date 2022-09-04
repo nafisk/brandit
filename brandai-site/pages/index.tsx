00c37291caeab33cefb33a4e64141fb27c2d1e05
Nafis Khan <nafisrizwank@gmail.com>
2022-09-04 12:11:58 -0400

    initialized next app

---
+++ brandai-site/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>BrandAI | AI Generated Marketing</title>
+        <meta
+          name='description'
+          content='Generated branding snippets for your product'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
